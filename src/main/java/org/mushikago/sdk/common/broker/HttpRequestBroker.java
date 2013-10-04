e138820af8087dc9fa1dcb593ba5c33c8b0eafda
suzukihiromihiromisuzuki <h.suzuki+github@mbrownie.com>
2013-10-04 16:57:33 +0900

    setRetry method add to MushikagoClient.

--- src/main/java/org/mushikago/sdk/common/broker/HttpRequestBroker.java
+++ src/main/java/org/mushikago/sdk/common/broker/HttpRequestBroker.java
@@ -21,50 +21,74 @@ import org.mushikago.sdk.common.util.ConnectInfo;
 
 /**
  * HTTPリクエストを代行。<br>
+ * 
  * @author miningbrownie
- *
+ * 
  */
 public class HttpRequestBroker extends RequestBroker {
-	
-	/**
-	 * ロガー。<br>
-	 */
-	private static Log log = LogFactory.getLog(HttpRequestBroker.class);
-	
-	/**
-	 * 指定された認証用オブジェクトを使用して、HttpRequestBrokerを構築します。<br>
-	 * @param credentials 認証用オブジェクト
-	 */
-	public HttpRequestBroker(Credentials credentials) {
-		
-		super(credentials);
-	}
-	
-	/**
-	 * HTTPリクエストを実行します。<br>
-	 * @param request リクエスト
-	 * @param ci 通信先情報
-	 * @return mushikagoサービスのAPIサーバから返されるJSONデータ
-	 * @throws RequestException リクエスト関係の例外が発生した場合
-	 * @throws BrokerException 通信関係の例外が発生した場合
-	 */
-	public JSONObject request(MushikagoRequest request, ConnectInfo ci) throws RequestException, BrokerException {
-		
-		try {
-			HttpRequestBase method = request.toHttpMethod(this.mushikagoAuth, ci);
-			HttpClient http = new DefaultHttpClient();
-			((AbstractHttpClient) http).setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(10, false)); 
-			log.info(String.format("request[%s],URI[%s]", request.getClass().toString(), method.getURI()));
-			
-			HttpResponse res = http.execute(method);
-			
-			String str = EntityUtils.toString(res.getEntity(), "UTF-8");
-			log.info(String.format("response[%s]", str));
-			JSONObject json = JSONObject.fromObject(str);
-			
-			return json;
-		}
-		catch(ClientProtocolException e){ throw new BrokerException(String.format("%s:%s", e.getClass().getName(), e.getMessage())); }
-		catch(IOException e) { throw new BrokerException(String.format("%s:%s", e.getClass().getName(), e.getMessage())); }
-	}
+
+  private static final int DEFAULT_RETRY_COUNT = 10;
+
+  /**
+   * Internal Error時のリトライ数
+   */
+  protected int retryCount = DEFAULT_RETRY_COUNT;
+  /**
+   * ロガー。<br>
+   */
+  private static Log log = LogFactory.getLog(HttpRequestBroker.class);
+
+  /**
+   * 指定された認証用オブジェクトを使用して、HttpRequestBrokerを構築します。<br>
+   * 
+   * @param credentials
+   *          認証用オブジェクト
+   */
+  public HttpRequestBroker(Credentials credentials) {
+
+    super(credentials);
+  }
+
+  /**
+   * HTTPリクエストを実行します。<br>
+   * 
+   * @param request
+   *          リクエスト
+   * @param ci
+   *          通信先情報
+   * @return mushikagoサービスのAPIサーバから返されるJSONデータ
+   * @throws RequestException
+   *           リクエスト関係の例外が発生した場合
+   * @throws BrokerException
+   *           通信関係の例外が発生した場合
+   */
+  public JSONObject request(MushikagoRequest request, ConnectInfo ci) throws RequestException, BrokerException {
+
+    try {
+      HttpRequestBase method = request.toHttpMethod(this.mushikagoAuth, ci);
+      HttpClient http = new DefaultHttpClient();
+      ((AbstractHttpClient) http).setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(this.retryCount, false));
+      log.info(String.format("request[%s],URI[%s]", request.getClass().toString(), method.getURI()));
+
+      HttpResponse res = http.execute(method);
+
+      String str = EntityUtils.toString(res.getEntity(), "UTF-8");
+      log.info(String.format("response[%s]", str));
+      JSONObject json = JSONObject.fromObject(str);
+
+      return json;
+    } catch (ClientProtocolException e) {
+      throw new BrokerException(String.format("%s:%s", e.getClass().getName(), e.getMessage()));
+    } catch (IOException e) {
+      throw new BrokerException(String.format("%s:%s", e.getClass().getName(), e.getMessage()));
+    }
+  }
+
+  public void setRetryCount(int retry) {
+    this.retryCount = retry;
+  }
+
+  public int getRetryCount() {
+    return this.retryCount;
+  }
 }
